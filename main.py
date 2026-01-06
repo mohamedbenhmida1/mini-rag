547f363d2cdbfe6639e09ccedd5aa2e34bef62fd
Mohamed Ben Hmida <mohamedbenhamida10@gmail.com>
2026-01-06 08:58:41 +0100

    changing welcome fct

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+
+from routes import base
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome_message():
-    return {"message": "Hello World"
-    }
+app.include_router(base.baserouter)
