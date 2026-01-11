cf2a7eac5549dc07e861c41db257d6fe295cbb10
Mohamed Ben Hmida <mohamedbenhamida10@gmail.com>
2026-01-11 23:35:17 +0100

    changes

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
 from .BaseController import BaseController
+from .ProjectController import ProjectController
 from fastapi import UploadFile
 from models import ResponseEnum
+import re
+import os
 
 class DataController(BaseController):
     
@@ -15,3 +18,35 @@ class DataController(BaseController):
                 return False, ResponseEnum.FILE_SIZE_EXCEEDED
            return True, ResponseEnum.FILE_VALID.value
             
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
+
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+
+        return new_file_path
+
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
--- src/routes/data.py
+++ src/routes/data.py
@@ -16,7 +16,8 @@ async def upload_data(project_id: str, file: UploadFile,
                     app_settings: settings = Depends(get_settings)):
 
     # validate the file properties
-    is_valid ,resulat_signal = DataController().validate_uploaded_file(file=file)
+    data_controller = DataController()
+    is_valid ,resulat_signal = data_controller.validate_uploaded_file(file=file)
 
 
     if not is_valid:
@@ -26,19 +27,31 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(
-        project_dir_path,
-        file.filename
-    )
+    file_path = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename,project_id=project_id)
+    
 
-    async with aiofiles.open(file_path, 'wb') as f:
-        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)    
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseEnum.UPLOAD_FAILED.value
+            }
+        )
+
     return JSONResponse(
-        content={
-            "signal": ResponseEnum.UPLOAD_SUCCESS.value
-        }
-    )
-
+            content={
+                "signal": ResponseEnum.UPLOAD_SUCCESS.value,
+                "file_path": file_path
+            }
+        )
 
     
