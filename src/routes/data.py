6b578f218b26f9f2a40c5345c17228b27707933e
Mohamed Ben Hmida <mohamedbenhamida10@gmail.com>
2026-01-12 16:10:06 +0100

    return uploaded file id

--- src/routes/data.py
+++ src/routes/data.py
@@ -27,8 +27,11 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_filepath(
-        orig_file_name=file.filename,project_id=project_id)
+    file_path, file_id = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+        
+        )
     
 
 
@@ -51,6 +54,7 @@ async def upload_data(project_id: str, file: UploadFile,
             content={
                 "signal": ResponseEnum.UPLOAD_SUCCESS.value,
                 "file_path": file_path
+                ,"file_id": file_id
             }
         )
 
