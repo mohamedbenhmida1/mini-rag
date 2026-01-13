50d9ebc096da68e4b55a7492428e1852a6cbd8d4
Mohamed Ben Hmida <mohamedbenhamida10@gmail.com>
2026-01-13 16:31:53 +0100

    Add MongoDB configuration and models, including Docker setup

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,6 +9,9 @@ class settings(BaseSettings):
     FILE_MAX_SIZE: int 
     FILE_DEFAULT_CHUNK_SIZE: int
 
+    MONGO_URI: str
+    MONGODB_DATABASE: str
+
     class Config:
         env_file = ".env" 
 
